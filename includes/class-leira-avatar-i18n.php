f7013445f391fee00aded02c276cbbbc44c6f7f5
Ariel Hernandez <arielhr1987@gmail.com>
2019-11-19 15:08:42 -0800

    plugin structure

---
+++ includes/class-leira-avatar-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://leira.dev
+ * @since      1.0.0
+ *
+ * @package    Leira_Avatar
+ * @subpackage Leira_Avatar/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Leira_Avatar
+ * @subpackage Leira_Avatar/includes
+ * @author     Ariel <arielhr1987@gmail.com>
+ */
+class Leira_Avatar_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'leira-avatar',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
